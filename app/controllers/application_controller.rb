e4a13ca5dce1d77dd8b18e148ece706b73b5b57f
ddlou <darren.lou@gmail.com>
2019-05-16 15:46:53 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < Sinatra::Base
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
-
       redirect '/login'
     end
   end
